76d385d073e92ee6a65fd00b3bec7b43ebf1fd60
BerkeKS <berkekasabah@gmail.com>
2025-04-01 20:27:38 +0300

    Add files for Language Core Web project

---
+++ LanguageCore/Scripts/Shared/_Layout.js
@@ -0,0 +1,12 @@
+﻿var Layout: {
+    Actions: {
+        toggleNav = function () {
+            var sideNavWidth = document.getElementById("mySidenav").style.width;
+            if (sideNavWidth == "0") {
+                sideNavWidth = "250px";
+            } else {
+                sideNavWidth = "0"
+            }
+        }
+    }
+}
