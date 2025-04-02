03484c9368e86d3840d92ca8c27a0b7801102286
BerkeKS <berkekasabah@gmail.com>
2025-04-02 19:07:16 +0300

    Add first script for layout

--- LanguageCore/Scripts/Shared/_Layout.js
+++ LanguageCore/Scripts/Shared/_Layout.js
@@ -1,6 +1,6 @@
-﻿var Layout = {
-    Actions: {
-        toggleNav = function () {
+﻿var Layout = new (function () {
+    var Actions = this.Actions = {
+        ToggleNav: function () {
             var sideNavWidth = document.getElementById("mySidenav").style.width;
             if (sideNavWidth == "0") {
                 sideNavWidth = "250px";
@@ -8,5 +8,5 @@
                 sideNavWidth = "0"
             }
         }
-    }
-}
+    };
+})();
